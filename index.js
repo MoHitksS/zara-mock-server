8a673924260fa8d58d8e6da8d9b4fcf631d6bf16
Mohitsharma <mohitkumarsharma40@gmail.com>
2022-12-21 14:53:04 +0530

    zara-mock-server

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
-const jsonServer = require('json-server')
-const cors = require('cors')
-const path = require('path')
+const jsonServer = require('json-server');
+const cors = require('cors');
+const path = require('path');
+const Monitor = require('ping-monitor')
+const myApi = new Monitor({
+    website: 'https://zara-origins.netlify.app/',
+    title: 'Zara',
+    interval: 3,
 
-const server = jsonServer.create()
-const router = jsonServer.router(path.join(__dirname, 'db.json'))
-const middlewares = jsonServer.defaults()
+    config: {
+      intervalUnits: 'minutes' // seconds, milliseconds, minutes {default}, hours
+    },
 
-server.use(cors())
-server.use(jsonServer.bodyParser)
-server.use(middlewares)
-server.use(router)
+    expect: {
+      statusCode: 200
+    }
+});
 
-const PORT = 9000
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const middlewares = jsonServer.defaults();
+
+server.use(cors());
+server.use(jsonServer.bodyParser);
+server.use(middlewares);
+server.use(router);
+
+const PORT = 9000;
 
 server.listen(PORT, () => {
   console.log(`JSON Server is running`)
